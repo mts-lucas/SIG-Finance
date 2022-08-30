220126f03b0b86245a82fc194d46cc551ec3c24f
Lucas Mateus <lmateus1067@outlook.com>
2022-08-30 14:49:28 -0300

    add telas

---
+++ M1_/cadastro_eco-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void menu_indic_eco(void) {
+
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+    printf("///          = = = = = Gerenciamento de investimentos = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cad. Perdil Economico                                     ///\n");
+    printf("///            2. Renda Variavel                                            ///\n");
+    printf("///            3. Criptomoeda                                               ///\n");
+    printf("///            4. Sobre o modulo                                            ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+
+}
