56b576e060c072325e76121857e1e23eb2c955fa
Lucas Mateus <lmateus1067@outlook.com>
2022-08-30 15:20:05 -0300

    cas tela M1 e tela M@

--- M1_/cadastro_eco-main.c
+++ M1_/cadastro_eco-main.c
@@ -6,12 +6,13 @@ void menu_indic_eco(void) {
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-    printf("///          = = = = = Gerenciamento de investimentos = = = = =             ///\n");
+    printf("///          = = = = = Perfil Economico do Usuario    = = = = =             ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Cad. Perdil Economico                                     ///\n");
-    printf("///            2. Renda Variavel                                            ///\n");
-    printf("///            3. Criptomoeda                                               ///\n");
-    printf("///            4. Sobre o modulo                                            ///\n");
+    printf("///            1. Cad. Perfil Economico                                     ///\n");
+    printf("///            2. Alt. Parametro de Perfil                                  ///\n");
+    printf("///            3. Deletar Parametro de Perfil                               ///\n");
+    printf("///            3. Visualizar Perfil                                         ///\n");
+    printf("///            5. Sobre o modulo                                            ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
