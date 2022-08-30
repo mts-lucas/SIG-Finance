03cc358a5d09f4a9e68c4774966af457e6c4915b
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-08-30 17:18:47 -0300

    tallys:implementação do esboço do menu principal desse modulo

---
+++ M2_/gastos_vari-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void menu_principal_gv(void);
+
+int main(void) {
+    
+    menu_principal_gv();
+
+    return 0;
+}
+
+void menu_principal_gv(void) {
+
+    printf("\n");
+
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = = = = Gerenciamento de Gastos Variaveis = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cadastrar gasto.                                          ///\n");
+    printf("///            2. Editar gasto.                                             ///\n");
+    printf("///            3. Excluir gastos.                                           ///\n");
+    printf("///            4. Checar gastos.                                            ///\n");
+    printf("///            5. Sobre o modulo.                                           ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
