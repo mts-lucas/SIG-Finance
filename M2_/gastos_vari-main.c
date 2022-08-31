66748e50e0378c1cd73defaf1847ba69e64a5f98
Tallys-Aureliano <tallysaureliano@outlook.com>
2022-08-30 21:43:03 -0300

    tallys: criando esboço e menu navegavel

--- M2_/gastos_vari-main.c
+++ M2_/gastos_vari-main.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void menu_principal_gv(void);
+char menu_principal_gv(void);
+void gerencia_menu_principal(void);
+char ler(char);
+void cadastrar(void);
+void editar(void);
+void excluir(void);
+void checar(void);
+void sobre(void);
+
+char ler(char x){
+  scanf("%c",&x);
+  getchar();
+  return x;
+}
 
 int main(void) {
     
-    menu_principal_gv();
+    // menu_principal_gv();
+    gerencia_menu_principal();
 
     return 0;
 }
 
-void menu_principal_gv(void) {
-
-    printf("\n");
-
+char menu_principal_gv(void) {
+    
+    char op;
+    system("clear||cls");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
     printf("///                                                                         ///\n");
-    printf("///           = = = = Gerenciamento de Gastos Variaveis = = = =             ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
     printf("///                                                                         ///\n");
     printf("///            1. Cadastrar gasto.                                          ///\n");
     printf("///            2. Editar gasto.                                             ///\n");
@@ -27,5 +42,113 @@ void menu_principal_gv(void) {
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
+    return ler(op);
+}
+
+void gerencia_menu_principal(void){
+    char op;
+    op = menu_principal_gv();
+    while (op != '0') {
+        
+        if (op == '1') {
+            cadastrar();
+        }
+
+        else if (op == '2') {
+            editar();
+            
+        }
+
+        else if (op == '3') {
+            excluir();
+            
+        }
+
+        else if (op == '4') {
+            checar();
+            
+        }
+
+        else if (op == '5') {
+            sobre();
+        }
+
+        op = menu_principal_gv();
+
+        }
+    }
+
+void cadastrar(void){
+    system("clear||cls");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///     Em desenvolvimento... Pressione qualquer tecla para voltar ao       ///\n");
+    printf("/// menu principal                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    getchar();
+}
+
+void editar(void){
+    system("clear||cls");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///     Em desenvolvimento... Pressione qualquer tecla para voltar ao       ///\n");
+    printf("/// menu principal                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    getchar();
+}
+
+void excluir(void){
+    system("clear||cls");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///     Em desenvolvimento... Pressione qualquer tecla para voltar ao       ///\n");
+    printf("/// menu principal                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    getchar();
+}
+
+void checar(void){
+    system("clear||cls");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///     Em desenvolvimento... Pressione qualquer tecla para voltar ao       ///\n");
+    printf("/// menu principal                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    getchar();
+}
+
+void sobre(void){
+    system("clear||cls");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Financas Domesticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           = == = = Gerenciamento de Gastos Variaveis = = = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///     Em desenvolvimento... Pressione qualquer tecla para voltar ao       ///\n");
+    printf("/// menu principal                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    getchar();
 }
