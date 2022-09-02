0355fb25dff11018401a29564941bf4ba01de801
Lucas Mateus <lmateus1067@outlook.com>
2022-09-02 17:27:52 -0300

    add tela principal do modulo 1

--- M1_/cadastro_eco-main.c
+++ M1_/cadastro_eco-main.c
@@ -1,5 +1,4 @@
-
-// arquivo referente ao modulo 1:
+//arquivo referente ao modulo 1:
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,223 +12,34 @@ void sobre_moradores(void);
 void deletar_morador(void);
 void altera_morador(void);
 
-char ler(char x)
-{
-  scanf("%c", &x);
+
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+    printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cadastro Morador                                          ///\n");
+    printf("///            2. Alterar dados de Morador                                  ///\n");
+    printf("///            3. Deletar Morador                                           ///\n");
+    printf("///            3. Visualizar Moradores                                      ///\n");
+    printf("///            5. Sobre o modulo                                            ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+
+}
+
+void interacao_menu_principal(void) {
+    while ()
+}
+
+
+char ler(char x){
+  scanf("%c",&x);
   getchar();
   return x;
-}
-
-int main(void)
-{
-
-  interacao_menu_principal();
-
-  return 0;
-}
-
-char menu_cad_moradores(void)
-{
-
-  char op;
-  system("clear||cls");
-  printf("\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
-  printf("///                                                                         ///\n");
-  printf("///            1. Cadastro de Morador                                       ///\n");
-  printf("///            2. Alterar dados de Morador                                  ///\n");
-  printf("///            3. Deletar Morador                                           ///\n");
-  printf("///            4. Visualizar Moradores                                      ///\n");
-  printf("///            5. Sobre o modulo                                            ///\n");
-  printf("///            0. Sair                                                      ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("\n");
-  return ler(op);
-}
-
-void interacao_menu_cad_morador(void)
-{
-
-  char op;
-  op = menu_cad_moradores();
-  while (op != '0')
-  {
-
-    if (op == '1')
-    {
-      cad_morador();
-    }
-
-    else if (op == '2')
-    {
-      altera_morador();
-    }
-
-    else if (op == '3')
-    {
-      deletar_morador();
-    }
-
-    else if (op == '4')
-    {
-      visualizar_morador();
-    }
-
-    else if (op == '5')
-    {
-      sobre_moradores();
-    }
-    else
-    {
-      printf("\n\t Opcao invalida. digite outra...");
-      getchar();
-    }
-
-    op = menu_cad_moradores();
-  }
-}
-
-
-void cad_morador(void)
-{
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =       Cadastro de Morador      = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Por favor informe os dados do morador:                         ///\n");
-  printf("///          Nome:                                                          ///\n");
-  printf("///          CPF:                                                           ///\n");
-  printf("///          Idade:                                                         ///\n");
-  printf("///          Ocupacao:                                                      ///\n");
-  printf("///          Renda Mensal:                                                  ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =       Cadastro de Morador      = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Morador cadastrado!:                                           ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-}
-
-void altera_morador(void)
-{
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =     Alterar dados de Morador   = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          De qual Morador deseja alterar os dados?                       ///\n");
-  printf("///          1. Lucas                                                       ///\n");
-  printf("///          2. Tallys                                                      ///\n");
-  printf("///          3. Flavius                                                     ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =     Alterar dados de Morador   = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Que dado deseja alterar deste Morador?                         ///\n");
-  printf("///          1. Nome                                                        ///\n");
-  printf("///          2. CPF                                                         ///\n");
-  printf("///          3. Idade                                                       ///\n");
-  printf("///          4. Ocupacao                                                    ///\n");
-  printf("///          5. Renda Mensal                                                ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =     Alterar dados de Morador   = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Dado Alterado!                                                 ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-}
-
-void deletar_morador(void)
-{
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
-  printf("///          = = = = =         Deletar Morador        = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Qual Morador deseja excluir da lista?                          ///\n");
-  printf("///          1. Lucas                                                       ///\n");
-  printf("///          1. Tallys                                                      ///\n");
-  printf("///          1. Flavius                                                     ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =         Deletar Morador        = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Morador deletado!                                              ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-}
-
-void visualizar_morador(void)
-{
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-  printf("///          = = = = =       Perfil de Moradores      = = = = =             ///\n");
-  printf("///          = = = = =       Visualizar Moradores     = = = = =             ///\n");
-  printf("///                                                                         ///\n");
-  printf("///          Estes são todos os moradores da residencia:                    ///\n");
-  printf("///          1. Lucas                                                       ///\n");
-  printf("///          1. Tallys                                                      ///\n");
-  printf("///          1. Flavius                                                     ///\n");
-  printf("///                                                                         ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
-}
-
-void sobre_moradores(void)
-{
-  system("clear||cls");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  printf("///                                                                         ///\n");
-  printf("///          = = = = =          SIG - FINANCE        = = = = =              ///\n");
-  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
-  printf("///          = = = = =              Sobre            = = = = =              ///\n");
-  printf("///                                                                         ///\n");
-  printf("///  Este modulo e dedicado a gerencia e controle das informacoes dos       ///\n");
-  printf("///  moradores da residencia.                                               ///\n");
-  printf("///                                                                         ///\n");
-  printf("///////////////////////////////////////////////////////////////////////////////\n");
-  getchar();
 }
