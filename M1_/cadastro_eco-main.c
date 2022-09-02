698a3a29777180ef6a4142cbba4c5083e1645285
Lucas Mateus <lmateus1067@outlook.com>
2022-09-02 17:27:52 -0300

    add tela principal do modulo 1

--- M1_/cadastro_eco-main.c
+++ M1_/cadastro_eco-main.c
@@ -1,21 +1,37 @@
+//arquivo referente ao modulo 1:
+
 #include <stdio.h>
 
 void menu_indic_eco(void) {
 
+
+    char op;
+    system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-    printf("///          = = = = = Perfil Economico do Usuario    = = = = =             ///\n");
+    printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Cad. Perfil Economico                                     ///\n");
-    printf("///            2. Alt. Parametro de Perfil                                  ///\n");
-    printf("///            3. Deletar Parametro de Perfil                               ///\n");
-    printf("///            3. Visualizar Perfil                                         ///\n");
+    printf("///            1. Cadastro Morador                                          ///\n");
+    printf("///            2. Alterar dados de Morador                                  ///\n");
+    printf("///            3. Deletar Morador                                           ///\n");
+    printf("///            3. Visualizar Moradores                                      ///\n");
     printf("///            5. Sobre o modulo                                            ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 
+}
+
+void interacao_menu_principal(void) {
+    while ()
+}
+
+
+char ler(char x){
+  scanf("%c",&x);
+  getchar();
+  return x;
 }
