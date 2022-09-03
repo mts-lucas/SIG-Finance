308c67b49e6f90277d7b448e0ea37c5b301bd6fb
Lucas Mateus <lmateus1067@outlook.com>
2022-09-02 21:36:19 -0300

    add subtelas basicas

--- M1_/cadastro_moradores_main.c
+++ M1_/cadastro_moradores_main.c
@@ -1,50 +1,110 @@
-//arquivo referente ao modulo 1:
+// arquivo referente ao modulo 1:
 
 #include <stdio.h>
 
-//void interacao_menu_principal(void); 
+// void interacao_menu_principal(void);
 char menu_cad_moradores(void);
 char ler(char);
- 
+void cad_morador(void);
+void visualizar_morador(void);
+void sobre_moradores(void);
 
-int main(void) {
 
-    //interacao_menu_principal(void); 
+int main(void){
 
-    return 0;
+  // interacao_menu_principal(void);
+
+  return 0;
 }
 
-char menu_cad_moradores(void) {
+char menu_cad_moradores(void){
 
-
-    char op;
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
-    printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Cadastro Morador                                          ///\n");
-    printf("///            2. Alterar dados de Morador                                  ///\n");
-    printf("///            3. Deletar Morador                                           ///\n");
-    printf("///            3. Visualizar Moradores                                      ///\n");
-    printf("///            5. Sobre o modulo                                            ///\n");
-    printf("///            0. Sair                                                      ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    return ler(op);
-
+  char op;
+  system("clear||cls");
+  printf("\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            1. Cadastro Morador                                          ///\n");
+  printf("///            2. Alterar dados de Morador                                  ///\n");
+  printf("///            3. Deletar Morador                                           ///\n");
+  printf("///            4. Visualizar Moradores                                      ///\n");
+  printf("///            5. Sobre o modulo                                            ///\n");
+  printf("///            0. Sair                                                      ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("\n");
+  return ler(op);
 }
 
-//void interacao_menu_principal(void) {
+// void interacao_menu_principal(void) {
 
 //}
 
-
 char ler(char x){
-  scanf("%c",&x);
+  scanf("%c", &x);
   getchar();
   return x;
+}
+
+void cad_morador(void){
+
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            .EM CONSTRUCAO                                               ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void altera_morador(void){
+
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            .EM CONSTRUCAO                                               ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void deletar_morador(void){
+
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            .EM CONSTRUCAO                                               ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void visualizar_morador(void){
+
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            .EM CONSTRUCAO                                               ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+}
+
+void sobre_moradores(void){
+
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///          = = = = =          SIG - FINANCE         = = = = =             ///\n");
+  printf("///          = = = = =       Perfil de Moradores     = = = = =              ///\n");
+  printf("///                                                                         ///\n");
+  printf("///            .EM CONSTRUCAO                                               ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
 }
