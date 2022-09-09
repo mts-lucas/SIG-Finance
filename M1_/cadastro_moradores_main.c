9cd4a03553b524602cc773fae90d5fb29b616c46
Lucas Mateus <lmateus1067@outlook.com>
2022-09-08 21:11:20 -0300

    gerando .h do M1

--- M1_/cadastro_moradores_main.c
+++ M1_/cadastro_moradores_main.c
@@ -2,17 +2,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "cadastro_moradores_main.h"
 
-// declarando as funcoes
-
-void interacao_menu_cad_morador(void);
-char menu_cad_moradores(void);
-char ler(void);
-void cad_morador(void);
-void visualizar_morador(void);
-void sobre_moradores(void);
-void deletar_morador(void);
-void altera_morador(void);
 
 // declarando as variaveis globais
 
