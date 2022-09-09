df2c29e0da456bea45a6bf97c77ec2d9aaf9e2cc
Lucas Mateus <lmateus1067@outlook.com>
2022-09-08 21:11:20 -0300

    gerando .h do M1

---
+++ M1_/cadastro_moradores_main.h
@@ -0,0 +1,8 @@
+void interacao_menu_cad_morador(void);
+char menu_cad_moradores(void);
+char ler(void);
+void cad_morador(void);
+void visualizar_morador(void);
+void sobre_moradores(void);
+void deletar_morador(void);
+void altera_morador(void);
