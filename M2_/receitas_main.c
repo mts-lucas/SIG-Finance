e5f44b790a761941a0789d6d97b4fee06d272e45
Lucas Mateus <lmateus1067@outlook.com>
2022-09-23 11:55:48 -0300

    .h m2

--- M2_/receitas_main.c
+++ M2_/receitas_main.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-char menu_principal_re(void);
-void gerencia_menu_principal(void);
-char ler_re(void);
-// char ler_res(void);
-void cadastrar_re(void);
-void editar_re(void);
-void excluir_re(void);
-void checar_re(void);
-void sobre_re(void);
+#include "receitas_main.h"
 
 char ler_re(void){
   printf("Selecione sua opção:");
