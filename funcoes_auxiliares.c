07ab333d9a2aa9f5c359df6aa3397bb927d5aa7b
Lucas Mateus <lmateus1067@outlook.com>
2022-10-04 13:45:46 -0300

    falta validar se é um numero

--- funcoes_auxiliares.c
+++ funcoes_auxiliares.c
@@ -3,8 +3,7 @@
 
 int meses[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-
-//criar uma função posterior que verificar se o numero digitado é um inteiro para então jogar os valores pra outras funções
+// criar uma função posterior que verificar se o numero digitado é um inteiro para então jogar os valores pra outras funções
 
 int main(void)
 {
@@ -28,7 +27,6 @@ int main(void)
         scanf("%d", &mes);
         printf("Informe o ano: ");
         scanf("%d", &ano);
-
     }
 
     dia_ano = dia_do_ano(dia, mes, ano);
@@ -85,26 +83,63 @@ int dataValida(int dd, int mm, int aa)
     return 1;
 }
 
-int dia_do_ano(int dd, int mm, int aa){
+int dia_do_ano(int dd, int mm, int aa)
+{
 
     dataValida(dd, mm, aa);
     int soma = 0;
-    for(int i = 0; i < mm - 1; i++){
+    for (int i = 0; i < mm - 1; i++)
+    {
         soma += meses[i];
     }
     soma += dd;
-    if ((mm > 2) && (bissexto(aa))) {
+    if ((mm > 2) && (bissexto(aa)))
+    {
         soma += 1;
     }
 
     return soma;
 }
 
-//Calcular a quantos dias foi um evento
+// Calcular a quantos dias foi um evento
 
-int quantos_dias(int dd, int mm, int aa){
+int quantos_dias(int dd, int mm, int aa)
+{
 
     printf("Precisa usar struct. ESTUDAR");
 
     return 0;
+}
+
+int validar_formato_data(char *data[11])
+{
+
+    if ((data[2] == " " || data[2] == "/" || data[2] == "-") && (data[5] == " " || data[5] == "/" || data[5] == "-"))
+    {
+
+        int d1, d2, dia;
+        d1 = data[0] - "0";
+        d2 = data[1] - "0";
+        dia = (d1 * 10) + d2;
+
+        int m1, m2, mes;
+        m1 = data[3] - "0";
+        m2 = data[4] - "0";
+        mes = (m1 * 10) + m2;
+
+        int a1, a2, a3, a4, ano;
+        a1 = data[6] - "0";
+        a2 = data[7] - "0";
+        a3 = data[8] - "0";
+        a4 = data[9] - "0";
+        mes = (a1 * 1000) + (a2 * 100) + (a3 * 10) + a4;
+
+        return dataValida(dia, mes, ano);
+    }
+
+    else
+    {
+
+        return 0;
+    }
 }
