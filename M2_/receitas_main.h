9218c6f68b6558fa723b429777ec63ca67e1ac29
Lucas Mateus <lmateus1067@outlook.com>
2022-11-09 14:28:36 -0300

    mod m2

--- M2_/receitas_main.h
+++ M2_/receitas_main.h
@@ -17,7 +17,6 @@ void gravarReceita(Receita* newreceita);
 char menu_principal_re(void);
 void gerencia_menu_principal(void);
 char ler(void);
-void cadastrar_re(void);
 void editar_re(void);
 void excluir_re(void);
 void checar_re(void);
