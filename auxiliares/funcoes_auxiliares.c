fa3b4b34173b56d63bde8590fdcbf3295cfc7718
Lucas Mateus <lmateus1067@outlook.com>
2022-11-12 20:13:22 -0300

    pausa

--- auxiliares/funcoes_auxiliares.c
+++ auxiliares/funcoes_auxiliares.c
@@ -74,21 +74,47 @@ int dia_do_ano(int dd, int mm, int aa) // by Flavius Gorgonio
 
     return soma;
 }
-// funcao que retorna o dia atual em forma de vetor de int
 
-int data_atual(void)
-{
-    struct tm *datetime;
-    int idia = datetime->tm_mday;
-    int imes = datetime->tm_mon + 1;
-    int iano = datetime->tm_year + 1900;
+// funcao que retorna o dia atual em forma de vetor de char
 
-    int data_hoje[] = {idia, imes, iano};
+// char data_atual(void)
+// {
+//     struct tm *datetime;
+//     int idia = datetime->tm_mday;
+//     int imes = datetime->tm_mon + 1;
+//     int iano = datetime->tm_year + 1900;
 
-    return data_hoje;
-}
+//     char diac[3];
+//     char mesc[3];
+//     char anoc[5];
 
+//     if (idia < 10)
+//     {
+//         char aux;
+//         sprintf(aux, "%d", idia);
+//         diac[0] = '0';
+//         diac[1] = aux;
+//     }
+//     else if (imes < 10)
+//     {
 
+//         char aux2;
+//         sprintf(aux2, "%d", imes);
+//         mesc[0] = '0';
+//         mesc[1] = aux2;
+//     }
+//     else
+//     {
+//         sprintf(diac, "%d", idia);
+//         sprintf(mesc, "%d", imes);
+//     }
+
+//     sprintf(anoc, "%d", iano);
+
+//     char data_hoje[] = {diac[0], diac[1], '/', mesc[0], mesc[1], '/', anoc[0], anoc[1], anoc[2], anoc[3]};
+
+//     return data_hoje;
+// }
 
 int validar_formato_data(char data[11])
 {
--- auxiliares/funcoes_auxiliares.h
+++ auxiliares/funcoes_auxiliares.h
@@ -1,7 +1,7 @@
 int dia_do_ano(int, int, int);
 int bissexto(int);
 int dataValida(int, int, int);
-int data_atual(void);
+// char data_atual(void);
 int validar_formato_data(char data[11]);
 int validar_letras(char nome[], int tam);
 int validar_dinheiro(char dinheiro[], int tam);
